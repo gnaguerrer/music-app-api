94d256c38dc6ba8e8c48b1fafc8acec8d8b7f55d
Gina Guerrero <gnaguerrer@gmail.com>
2024-01-08 17:26:54 -0500

    feat: update profile controller

--- controllers/user.js
+++ controllers/user.js
@@ -140,8 +140,42 @@ const getProfile = async (req, res) => {
   }
 };
 
+const updateProfile = async (req, res) => {
+  const profileId = req?.params?.profileId;
+  if (!mongoose.isValidObjectId(profileId)) {
+    return res.status(400).json({
+      error: true,
+      message: 'Missing or invalid profile id',
+      data: null
+    });
+  }
+  try {
+    const user = await User.findById(profileId)
+      .select({ password: 0, role: 0, __v: 0 })
+      .exec();
+    if (!user) {
+      return res.status(404).json({
+        error: true,
+        message: 'Profile not found. Try it later',
+        data: null
+      });
+    }
+    return res.status(200).json({
+      message: 'Login successfully',
+      data: user
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: 'Unable to get user profile. Try it later',
+      data: null
+    });
+  }
+};
+
 export default {
   signUp,
   login,
-  getProfile
+  getProfile,
+  updateProfile
 };
