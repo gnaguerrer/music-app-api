95a7218de38b656d3f640a4423882110ab601347
Gina Guerrero <gnaguerrer@gmail.com>
2024-01-09 17:13:47 -0500

    feat: add user routes

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,11 @@ const router = Router();
 router.post('/signUp', UserController.signUp);
 router.post('/login', UserController.login);
 router.get('/profile/:profileId', middlewares.auth, UserController.getProfile);
+router.put(
+  '/profile/:profileId',
+  middlewares.auth,
+  UserController.updateProfile
+);
+router.put('/profile/', middlewares.auth, UserController.updateProfile);
 
 export default router;
