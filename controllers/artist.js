d0c6ee33c7c42f82dedb51fcf4f7c2f47b0bce6e
Gina Guerrero <gnaguerrer@gmail.com>
2024-01-14 15:56:32 -0500

    feat: get artist by id

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Artist from '../models/artist.js';
 
 const saveArtist = async (req, res) => {
@@ -25,6 +26,38 @@ const saveArtist = async (req, res) => {
   }
 };
 
+const getArtist = async (req, res) => {
+  const artistId = req.params?.id;
+  if (!mongoose.isValidObjectId(artistId)) {
+    return res.status(400).json({
+      message: 'Invalid id',
+      data: null,
+      error: true
+    });
+  }
+  try {
+    const artist = await Artist.findById(artistId);
+    if (artist) {
+      return res.status(200).json({
+        message: 'Artist found successfully',
+        data: artist
+      });
+    }
+    return res.status(404).json({
+      error: true,
+      message: 'Unable to find artist. Try it later',
+      data: null
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: 'Unable to get artist. Try it later',
+      data: null
+    });
+  }
+};
+
 export default {
-  saveArtist
+  saveArtist,
+  getArtist
 };
--- routes/artist.js
+++ routes/artist.js
@@ -4,7 +4,7 @@ import middlewares from '../middlewares/index.js';
 
 const router = Router();
 
-// router.get('/', ArtistController.routeTest);
 router.post('/', middlewares.auth, ArtistController.saveArtist);
+router.get('/:id', middlewares.auth, ArtistController.getArtist);
 
 export default router;
